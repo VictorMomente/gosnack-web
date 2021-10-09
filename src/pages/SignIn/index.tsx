c82902141ac37ec5df54e74a1b927fa862afe653
VictorMomente <victor.h.momente@gmail.com>
2021-10-09 02:24:48 -0300

    feat: add login page and configure

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { FiLogIn } from 'react-icons/fi'
+import logoImg from '../../assets/logo.svg'
 import { Conteiner, Content, Background } from './styles'
 
 const SignIn: React.FC = () => (
   <Conteiner>
-    <Content></Content>
-    <Background></Background>
+    <Content>
+      <img src={logoImg} alt="goSnack" />
+      <form>
+        <h1>Faça seu login</h1>
+        <input placeholder="E-mail" />
+        <input placeholder="Senha" type="password" />
+        <button type="submit">Entrar</button>
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+      <a href="login">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
   </Conteiner>
 )
 
