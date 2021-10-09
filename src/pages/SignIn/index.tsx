ca47406c202323590745a4f7aff27c28c46db595
VictorMomente <victor.h.momente@gmail.com>
2021-10-08 22:11:53 -0300

    feat: add sign in page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Conteiner, Content, Background } from './styles'
+
+const SignIn: React.FC = () => (
+  <Conteiner>
+    <Content></Content>
+    <Background></Background>
+  </Conteiner>
+)
+
+export default SignIn
