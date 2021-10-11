3399ff36f3ffeac3b89bda42e1139077919156ef
VictorMomente <victor.h.momente@gmail.com>
2021-10-11 00:48:59 -0300

    feat: add styles on forms

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,17 +1,36 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Container = styled.div`
+interface ConteinerProps {
+  isFocused: boolean
+  isFilled: boolean
+}
+
+export const Container = styled.div<ConteinerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
   display: flex;
+
+  border: 2px solid #232129;
   color: #666360;
 
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     background: transparent;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,34 +2,42 @@ import React from 'react'
 import { FiArrowDownLeft, FiMail, FiUser, FiLock } from 'react-icons/fi'
 import logoImg from '../../assets/logo.svg'
 import { Conteiner, Content, Background } from './styles'
+import { Form } from '@unform/web'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
-const SignUp: React.FC = () => (
-  <Conteiner>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="goSnack" />
-      <form>
-        <h1>Faça seu cadastro</h1>
-        <Input name="name" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          placeholder="Senha"
-          type="password"
-        />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data)
+  }
 
-        <Button type="submit">Cadastrar</Button>
-      </form>
-      <a href="login">
-        <FiArrowDownLeft />
-        Voltar para login
-      </a>
-    </Content>
-  </Conteiner>
-)
+  return (
+    <Conteiner>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="goSnack" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            placeholder="Senha"
+            type="password"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="login">
+          <FiArrowDownLeft />
+          Voltar para login
+        </a>
+      </Content>
+    </Conteiner>
+  )
+}
 
 export default SignUp
